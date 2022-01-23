bc68c10c8d61bf58ad13206a03589e8274be4e9b
miao0013 <96262905+miao0013@users.noreply.github.com>
2022-01-22 21:56:47 -0500

    create-demo

---
+++ js/index.js
@@ -0,0 +1,18 @@
+let x = 15;
+let y = 5;
+
+/*
+// concatinating using operator +
+console.log('x = 15, y = 5');
+console.log('x + y = ' + (x + y)); // 20
+console.log('x - y = ' + (x - y)); // 10
+console.log('x * y = ' + (x * y)); // 75
+console.log('x / y = ' + (x / y)); // 3
+console.log('x % y = ' + (x % y)); // 0
+*/
+
+// interpolating variables into string
+console.log(`x = 15, y = 5`);
+console.log(`x + y = ${(x + y)}`); // 20
+
+alert('Hello from JavaScript file!');
